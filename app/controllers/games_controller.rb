2aa9618645957fe0908ea496ec92648b0e5ac389
amy hastings <amyh364@outlook.com>
2024-04-22 15:50:40 +0100

    challenge 3

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,37 @@
+require "json"
+require "open-uri"
+require "uri"
+
 class GamesController < ApplicationController
   def new
+    alphabet = ("A".."Z").to_a
+    @letters = alphabet.sample(10) # change this to the dynamic version you have in the old challenge
   end
 
   def score
+      # raise
+    guess = params[:guess]
+    letters = params[:letters]
+    @score = 0
+
+    url = "https://wagon-dictionary.herokuapp.com/#{guess}"
+    word_check = URI.open(url).read
+
+    @word = JSON.parse(word_check)
+    # implement here the logic to compute the score
+    guess.each_char do |letter|
+      unless letters.include?(letter.upcase)
+        @wrong_letters = true
+        break
+      end
+      @wrong_letters = false
+    end
+    @not_eng = true if @word["found"] == false
+    if @wrong_letters != true && @not_eng != true
+      @score = guess.length
+    end
   end
+
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/games", to: "controller#games"
+  get "/new", to: "games#new"
+  get "/score", to: "games#score"
+  # get "/TODO", to: "games#new"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
